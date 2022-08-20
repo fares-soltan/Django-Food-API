299503b87892409689567d26b217ae7afe4a6afa
Fares mohamed <eng.fares.alsayed@gmail.com>
2022-08-20 04:34:45 +0200

    commit message

--- api/settings.py
+++ api/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'resturant',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
