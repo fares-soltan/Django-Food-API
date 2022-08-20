3eb56b075377f1d142cdb0124aeeb8c8bcc7bee8
Fares mohamed <eng.fares.alsayed@gmail.com>
2022-08-20 04:36:25 +0200

    commit message

--- api/settings.py
+++ api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2w#tzwhs)_plf^mz3!9s26a_r26*dk$nklzrk!@8g-i76_*m6q'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
