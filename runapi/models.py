34a9d3544b29a9568537f0dd8fa5c3907a34e8c2
Fares mohamed <eng.fares.alsayed@gmail.com>
2022-08-21 02:11:23 +0200

    first commit

--- runapi/models.py
+++ runapi/models.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 from django.db import models
 
 
@@ -5,12 +6,12 @@ class Users(models.Model):
     UserName = models.CharField(max_length=200, null=False, blank=False)
     UserPass = models.CharField(max_length=50, blank=False)
     UserEmail = models.CharField(max_length=100, null=False, blank=False)
-    UserPhone = models.IntegerField()
-    UserAdress = models.TextField()
-    UserInnerName = models.CharField(max_length=200)
-    u_Admin = models.BooleanField(default=0)
-    Staff = models.BooleanField(default=1)
-    UserPoints = models.IntegerField(default=0,)
+    UserPhone = models.IntegerField(null=True, blank=TRUE)
+    UserAdress = models.TextField(null=True, blank=TRUE)
+    UserInnerName = models.CharField(max_length=200, null=True, blank=TRUE)
+    u_Admin = models.BooleanField(blank=TRUE, default=0)
+    Staff = models.BooleanField(blank=TRUE, default=1)
+    UserPoints = models.IntegerField(default=0)
 
     def __str__(self):
         return self.UserName
